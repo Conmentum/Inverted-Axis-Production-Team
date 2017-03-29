d086c21c354456f5f65ffcc97394bd401f9c98d2
DESKTOP-Q8TLR3L\Keaghan's PC <keaghan.b.potts@gmail.com>
2017-03-29 18:19:07 +1000

    +Added functionality for the enemies and some functionality for the inventory

---
+++ AI Virus Evolved/Assets/Assets/Scripts/InventoryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour {
+
+    public int lives = 30;
+    //Change Based On Level
+    public int currency = 100;
+
+    public Text currencyText;
+    public Text livesText;
+
+    public void LoseLife()
+    {
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+        //show gameover screen
+    }
+
+    void Update()
+    {
+        //This doesn't actually need to update the text every frame.
+        currencyText.text = "Money: $" + currency.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+    }
+}
---
+++ AI Virus Evolved/Assets/Assets/Scripts/Projectile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public Transform target;
+    public float damage;
+    public float radius;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dirToTarget = target.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+
+        if (dirToTarget.magnitude <= distThisFrame)
+        {
+            ProjectileHit();
+        }
+        else
+        {
+            transform.Translate(dirToTarget.normalized * distThisFrame, Space.World);
+            Quaternion targetRot = Quaternion.LookRotation(dirToTarget);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, Time.deltaTime * 5);
+        }
+	}
+
+    void ProjectileHit()
+    {
+        if (radius == 0)
+        {
+            target.GetComponent<EnemyBehaviour>().TakeDamage(damage);
+        }
+        else
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+
+            foreach(Collider cc in colliders)
+            {
+                EnemyBehaviour e = cc.GetComponent<EnemyBehaviour>();
+                if(e != null)
+                {
+                    e.GetComponent<EnemyBehaviour>().TakeDamage(damage);
+                }
+            }
+        }
+        //add effect here
+        Destroy(gameObject);
+    }
+}
