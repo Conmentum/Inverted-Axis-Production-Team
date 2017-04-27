6a38d923f3095cd3ab87cbcb5c89fcaaf3a8f9f5
Victor Weidar <Victor.weidar@gmail.com>
2017-04-27 10:45:59 +1000

    began working on audiomanager

---
+++ Assets/stuff/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+    public AudioClip clip;
+
+    public float Volume = 0.7f;
+    public float Pitch = 1f;
+
+    private AudioSource source;
+
+    public void SetSource (AudioSource _Source)
+    {
+        source = _Source;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+        source.volume = Volume;
+        source.pitch = Random.Range(0.5f, 1f);
+        source.Play();
+    }
+}
+
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField]
+    Sound[] sounds;
+
+    //create a singleton
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+
+}
