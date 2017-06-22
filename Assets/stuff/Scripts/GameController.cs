d5b3437559c28beb9b81508ac31e061043c94db6
Victor Weidar <Victor.weidar@gmail.com>
2017-06-22 18:57:06 +1000

    reworked wavespawner #1

--- Assets/stuff/Scripts/GameController.cs
+++ Assets/stuff/Scripts/GameController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	public List<GameObject> enemies;
+
     public GameObject Tutor_UI;
     public GameObject Tutor_UI1;
     public GameObject tutor_UI2;
-    public bool NewWave;
 	// Use this for initialization
 	void Start () {
-        NewWave = false;
         if (Tutor_UI == null)
         {
             return;
@@ -21,29 +19,8 @@ public class GameController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (NewWave == true)
-        {
-            FindObjectOfType<EnemySpawner>().SpawningSystem();
-            if (FindObjectOfType<EnemySpawner>() == null)
-            {
-                NewWave = false;
-                return;
-            }
-        }
-    }
-    public void StartNewwWave()
-    {
-        FindObjectOfType<EnemySpawner>().waveActive = true;
-        NewWave = true;
-    }       
-	public void EnemySpawned(GameObject _new){
-		enemies.Add (_new);
-	}
 
-	public void EnemyDeath(GameObject _Death)
-	{
-		enemies.Remove (_Death);
-	}
+    }
 
     public void UI_transition1()
     {
---
+++ Assets/stuff/Scripts/WaveSpawner.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    };
+
+    //look into ways of having more than one enemy per wave
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int AmountOfEnemies;
+        public float SpawnRate;
+    }
+
+    public Wave[] waves;
+    SpawnState state = SpawnState.COUNTING;
+
+    private int NextWave = 0;
+
+    public float TimeBetweenWaves = 3f;
+    public float WaveCountdown;
+
+    public Transform SpawnPoint;
+
+    private float SearchCountdown = 1f;
+
+
+    void Start()
+    {
+        WaveCountdown = TimeBetweenWaves;
+
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if(!EnemyIsAlive())
+            {
+                Debug.Log("wave complete");
+            }
+
+            else
+            {
+                return;
+            }
+        }
+
+        if(WaveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[NextWave]));
+            }
+
+            else
+            {
+                WaveCountdown -= Time.deltaTime;
+            }
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        SearchCountdown -= Time.deltaTime;
+
+        if(SearchCountdown <= 0f)
+        {
+            SearchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null || GameObject.FindGameObjectWithTag("Aerial") == null || GameObject.FindGameObjectWithTag("Boss") == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator  SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.AmountOfEnemies; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.SpawnRate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Instantiate(_enemy, SpawnPoint.position, Quaternion.identity);
+    }
+}
